a43457a90f9ba1f495c8f7ac5800611785c2388a
embersee <philipp@shiyanov.com>
2023-06-15 02:41:58 +0300

    swap encodeURI to Component

--- lib/sendTelegram.ts
+++ lib/sendTelegram.ts
@@ -2,7 +2,7 @@ export async function SendTelegram(reciever: string, message: string) {
   const res = await fetch(
     `https://api.telegram.org/bot${
       process.env.BOT_TOKEN
-    }/sendMessage?chat_id=${reciever}&text=${encodeURI(
+    }/sendMessage?chat_id=${reciever}&text=${encodeURIComponent(
       message
     )}&parse_mode=MarkdownV2`
   );
