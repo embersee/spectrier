25f4b0c1e2849ef5b6b4bae8f5d240efe1fdd933
embersee <philipp@shiyanov.com>
2023-06-14 20:49:47 +0300

    fix: missing key prop in order columns

--- components/table/order-columns.tsx
+++ components/table/order-columns.tsx
@@ -1,201 +1,201 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
-import { Checkbox } from "@/components/ui/checkbox"
-import { DataTableColumnHeader } from "@/components/table/column-header"
-import { Category, Product } from "@/lib/db/schema"
-import { DataTableRowActions } from "./row-action"
-import { Badge } from "../ui/badge"
-import { Orders } from "@/types/orders"
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { DataTableColumnHeader } from "@/components/table/column-header";
+import { Category, Product } from "@/lib/db/schema";
+import { DataTableRowActions } from "./row-action";
+import { Badge } from "../ui/badge";
+import { Orders } from "@/types/orders";
 
 export const orderColumns: ColumnDef<Orders>[] = [
-	{
-		id: "select",
-		header: ({ table }) => (
-			<Checkbox
-				checked={table.getIsAllPageRowsSelected()}
-				onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-				aria-label="Select all"
-				className="translate-y-[2px]"
-			/>
-		),
-		cell: ({ row }) => (
-			<Checkbox
-				checked={row.getIsSelected()}
-				onCheckedChange={(value) => row.toggleSelected(!!value)}
-				aria-label="Select row"
-				className="translate-y-[2px]"
-			/>
-		),
-		enableSorting: false,
-		enableHiding: false,
-	},
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
 
-	{
-		accessorKey: "id",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="ID" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className=" truncate font-medium">{row.original.order.id}</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "createdAt",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Создан" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						{new Date(
-							row.original.order.createdAt as string
-						).toLocaleDateString("ru-RU")}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "description",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Пользователь" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						@{row.original.user.username}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "comment",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Комментарий" />
-		),
-		cell: ({ row }) => {
-			const comment = row.original.order.comment as string
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						{comment || "Без комментарий"}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "orderStatus",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Order Status" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						{row.original.order.orderStatus}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "paymentType",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Payment Type" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						{row.original.order.paymentType}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "paymentStatus",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Payment Status" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						{row.original.order.paymentStatus}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "address",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Адрес" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						{row.original.order.address}
-					</span>
-				</div>
-			)
-		},
-	},
-	{
-		accessorKey: "product",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Product" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="space-y-2 whitespace-nowrap w-32 ">
-					<div className="flex justify-between px-1">
-						<p className=" truncate font-medium">Название</p>
-						<p className=" truncate font-medium">Кол.</p>
-					</div>
-					{row.original.product.map((item) => (
-						<div className="flex flex-col p-1 border rounded-lg">
-							<div className=" truncate font-medium flex justify-between">
-								<p>{item.name}</p>
-								<p>{item.quantity}</p>
-							</div>
-						</div>
-					))}
-				</div>
-			)
-		},
-		enableSorting: false,
-		enableHiding: false,
-	},
-	{
-		accessorKey: "totalSum",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Сумма" />
-		),
-		cell: ({ row }) => {
-			return (
-				<div className="flex space-x-2">
-					<span className="max-w-[200px] truncate font-medium">
-						₽{row.original.order.totalSum}
-					</span>
-				</div>
-			)
-		},
-	},
+  {
+    accessorKey: "id",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="ID" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className=" truncate font-medium">{row.original.order.id}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Создан" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            {new Date(
+              row.original.order.createdAt as string
+            ).toLocaleDateString("ru-RU")}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Пользователь" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            @{row.original.user.username}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "comment",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Комментарий" />
+    ),
+    cell: ({ row }) => {
+      const comment = row.original.order.comment as string;
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            {comment || "Без комментарий"}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "orderStatus",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Order Status" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            {row.original.order.orderStatus}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "paymentType",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Payment Type" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            {row.original.order.paymentType}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "paymentStatus",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Payment Status" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            {row.original.order.paymentStatus}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "address",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Адрес" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            {row.original.order.address}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "product",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Product" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="space-y-2 whitespace-nowrap w-32 ">
+          <div className="flex justify-between px-1">
+            <p className=" truncate font-medium">Название</p>
+            <p className=" truncate font-medium">Кол.</p>
+          </div>
+          {row.original.product.map((item) => (
+            <div key={item.id} className="flex flex-col p-1 border rounded-lg">
+              <div className=" truncate font-medium flex justify-between">
+                <p>{item.name}</p>
+                <p>{item.quantity}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    },
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "totalSum",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Сумма" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[200px] truncate font-medium">
+            ₽{row.original.order.totalSum}
+          </span>
+        </div>
+      );
+    },
+  },
 
-	{
-		id: "actions",
-		cell: ({ row }) => <DataTableRowActions id={row.original.order.id} />,
-	},
-]
+  {
+    id: "actions",
+    cell: ({ row }) => <DataTableRowActions id={row.original.order.id} />,
+  },
+];
