da6895b3a1129b694b5a4be7142b37719acf25d7
embersee <philipp@shiyanov.com>
2023-07-09 19:57:14 +0300

    telegram webapp expand on locales page

--- app/webapp/locales/page.tsx
+++ app/webapp/locales/page.tsx
@@ -10,12 +10,13 @@ export default async function Locales() {
   useEffect(() => {
     if (window == undefined) return;
     Telegram.WebApp.ready();
+    Telegram.WebApp.expand();
   }, []);
   return (
-    <div className="flex flex-col m-auto justify-center items-center h-screen">
+    <div className="flex flex-col m-auto justify-center items-center h-screen font-semibold">
       <h1>Выберите язык</h1>
       <h1>Тілді таңдаңыз</h1>
-      <div className="flex gap-4">
+      <div className="flex gap-4 mt-5">
         <Button onClick={() => router.push("/webapp")}>Русский</Button>
         <Button onClick={() => router.push("/kz/webapp")}>Қазақ</Button>
       </div>
